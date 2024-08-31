321264d0e02fa722df63638d471d0bf6b07581e9
BenjaminRC-TESCH <benjamin_rc@tesch.edu.mx>
2024-08-31 17:43:01 -0600

    first commit

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const envorinment = {
+  production: false,
+  firebase: {
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+  },
+};
